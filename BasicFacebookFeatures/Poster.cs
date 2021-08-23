634508131e4c83d49438640aeeec0be74f4994f4
Lior Shtaimberg <34008861+shlior7@users.noreply.github.com>
2021-08-23 23:55:51 +0300

    PostingProxy

---
+++ BasicFacebookFeatures/Poster.cs
@@ -0,0 +1,14 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures
+{
+    interface Poster
+    {
+        Status PostStatus(string i_Message);
+    }
+}
